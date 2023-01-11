bc76e75954786582e4a8392c14eb3e8d0c1a5e3a
Francis Vagner da Luz <francisvagnerdaluz@gmail.com>
2023-01-11 08:57:52 -0300

    Primeira versão

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,43 @@
+import styles from './Navbar.module.scss';
+import classNames from 'classnames';
+import {
+  RiShoppingCart2Line,
+  RiShoppingCartFill
+} from 'react-icons/ri';
+import Busca from '../Busca';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+
+const iconeProps = {
+  color: 'white',
+  size: 24
+}
+
+export default function Navbar() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  return (
+    <nav className={styles.nav}>
+      <h1 className={styles.titulo}>Portifólio</h1>
+      <div className={styles.links}>
+        <div>
+          <Link to='/' className={classNames(styles.link, {
+            [styles.selected]: location.pathname === '/'
+          })}>
+            Página inicial
+          </Link>
+        </div>
+      </div>
+      <div className={styles.busca}>
+        <Busca />
+      </div>
+      <div className={styles.icones}>
+        <Link to="/carrinho">
+          {location.pathname === '/carrinho'
+            ? <RiShoppingCartFill {...iconeProps} />
+            : <RiShoppingCart2Line {...iconeProps} />
+          }
+        </Link>
+      </div>
+    </nav>
+  )
+}
